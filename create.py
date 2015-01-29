36a96749ed3f4b1e040eda8b02836efd926f2cd5
Sage Vann <sagevann@gmail.com>
2015-01-28 21:25:06 -0800

    one more try with a new port

--- create.py
+++ create.py
@@ -140,5 +140,6 @@ def get_class(classname):
 #
 
 
-if __name__ == '__main__':
-    app.run(debug=True, port=33507)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
